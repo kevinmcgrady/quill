bc3bc8b757e96bcb12ec0a465e078ae7287a2bea
Kev McGrady <kevinmcgrady47@gmail.com>
2024-01-03 21:38:54 +0000

    @feat/pdf-renderer

--- src/app/dashboard/[fileId]/page.tsx
+++ src/app/dashboard/[fileId]/page.tsx
@@ -33,7 +33,7 @@ export default async function Page({ params }: PageProps) {
       <div className='mx-auto w-full max-w-8xl grow lg:flex xl:px-2'>
         <div className='flex-1 xl:flex'>
           <div className='px-4 py-6 sm:px-6 lg:pl-8 xl:flex-1 xl:pl-6'>
-            <PDFRenderer />
+            <PDFRenderer fileURL={file.url} />
           </div>
         </div>
 
